5db6e6b36f8458080d73010683969ad083367397
Bob Owen <bob@bobowen.org>
2019-12-07 17:41:15 -0500

    Update colors and favicon

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,17 @@
 import Typography from "typography"
 import GithubTheme from "typography-theme-github"
 
+GithubTheme.overrideThemeStyles = ({ rhythm }) => ({
+  blockquote: {
+    borderLeft: "4px solid white",
+    color: "white",
+    marginTop: 0,
+    marginRight: 0,
+    marginLeft: 0,
+    paddingLeft: `calc(${rhythm(1 / 2)} - 1px)`,
+  },
+})
+
 const typography = new Typography(GithubTheme)
 
 // Hot reload typography in development.
